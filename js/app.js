09dd20b265587e84b5eba22ad83de1be294c88c7
darcycervh <darcycervera@gmail.com>
2017-11-19 22:14:00 -0500

    arreglando indentacion con eslint

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 var inputBox = document.getElementById('input');
-inputBox.addEventListener('click',function(){
-    inputBox.style.marginLeft ='0';
-    var inputBoxParent = document.getElementById('input').parentNode;
-    inputBoxParent.classList.add('input-box-message');
-    var button = document.createElement('button');
-    var buttonContent = document.createTextNode('Guardar');
-    button.classList.add('button-input');
-    button.appendChild(buttonContent);
-    inputBoxParent.appendChild(button);
+inputBox.addEventListener('click', function() {
+  inputBox.style.marginLeft = '0';
+  var inputBoxParent = document.getElementById('input').parentNode;
+  inputBoxParent.classList.add('input-box-message');
+  var button = document.createElement('button');
+  var buttonContent = document.createTextNode('Guardar');
+  button.classList.add('button-input');
+  button.appendChild(buttonContent);
+  inputBoxParent.appendChild(button);
 
-    var contentXDiv = document.createElement('a');
-    var drawX = document.createTextNode('X');
-    contentXDiv.classList.add('equis');
-    contentXDiv.appendChild(drawX);
-    inputBoxParent.appendChild(contentXDiv);
-
+  var contentXDiv = document.createElement('a');
+  var drawX = document.createTextNode('X');
+  contentXDiv.classList.add('equis');
+  contentXDiv.appendChild(drawX);
+  inputBoxParent.appendChild(contentXDiv);
 });
 
-//var buttonInDocument = document.body.children[2].children[1];
-button.addEventListener('click', function(){
-    alert('holi');
+// var buttonInDocument = document.body.children[2].children[1];
+button.addEventListener('click', function() {
+  alert('holi');
 });
 
 /*
